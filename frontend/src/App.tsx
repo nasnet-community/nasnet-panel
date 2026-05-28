a69e53b026b6d7f5ce575f93182d2e2d6b11cf1c
Mo Jahani <onlymaj@users.noreply.github.com>
2026-05-28 11:01:54 +0300

    feat(fe): add plugins marketplace page (#199)
    * add plugins marketplace page
    
    * add e2e tests for plugins marketplace page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,6 +15,7 @@ import { VPNPage } from './routes/VPNPage';
 import { WirelessPage } from './routes/WirelessPage';
 import { LogsPage } from './routes/LogsPage';
 import { UpdatesPage } from './routes/UpdatesPage';
+import { PluginsPage } from './routes/PluginsPage';
 import { DHCPPage } from './routes/DHCPPage';
 import { DNSPage } from './routes/DNSPage';
 import { FirewallPage } from './routes/FirewallPage';
@@ -62,6 +63,7 @@ export function App() {
                   <Route path="lan" element={<DHCPPage />} />
                   <Route path="dns" element={<DNSPage />} />
                   <Route path="firewall" element={<FirewallPage />} />
+                  <Route path="plugins" element={<PluginsPage />} />
                   <Route path="help" element={<HelpPage />} />
                 </Route>
                 <Route
