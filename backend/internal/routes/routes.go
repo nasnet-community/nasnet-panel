7ff8ceb8bcd8d907d5e902c9bf141ccfe6ce77bc
Yahsar <yashar2004@gmail.com>
2026-09-03 00:08:21 +0300

    feat(be): Added an endpoint to flush DNS cache

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -106,6 +106,7 @@ func RegisterRoutes(e *echo.Echo) {
 	dnsGroup.POST("/change", handler.HandleChangeDNS)
 	dnsGroup.POST("/family", handler.HandleSetFamilyDNS)
 	dnsGroup.POST("/adblock", handler.HandleSetAdBlock)
+	dnsGroup.DELETE("/cache", handler.HandleFlushDNSCache)
 	dnsGroup.POST("/reset", handler.HandleResetDNS)
 
 	vpnGroup := e.Group("/api/vpn")
